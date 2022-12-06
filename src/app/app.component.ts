161c7c649c4d38150f15243b90db2c0171c56f47
Jorge Pires <jorgeguerrapires@yahoo.com.br>
2022-12-06 07:55:24 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//TensorFlow.js: pre-trained model
+import * as toxicity from '@tensorflow-models/toxicity';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+
+  ngOnInit() {
+
+    this.moderate();
+
+  }
+
+
+  moderate() {
+    //Tolerance
+    const threshold = 0.9;
+
+    //calling a method from our third-party library
+    toxicity.load(threshold, []).then(model => {
+
+      //now we use our model, parametrized
+      model.classify("You suck").then(predictions => {
+        //Printing out the final result
+        console.log(predictions);
+        // console.log(predictions[0].results[0].match);
+      })
+
+    })
+  }//end of moderate()
+
+
+
 }
