b2908a6e1593b46be8b89076b701da863641f3ac
Jorge Pires <jorgeguerrapires@yahoo.com.br>
2022-12-07 13:45:48 -0300

    user interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { Component, OnInit } from '@angular/core';
 import * as toxicity from '@tensorflow-models/toxicity';
 
 
+import { FormControl } from '@angular/forms';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,26 +16,45 @@ import * as toxicity from '@tensorflow-models/toxicity';
 export class AppComponent implements OnInit {
   title = 'client';
 
+  moderationText = new FormControl('');
+
+  moderation = "I am thinking, just a moment 🤖🤖";
+
+
 
   ngOnInit() {
 
-    this.moderate();
+    // this.moderate();
 
   }
 
 
   moderate() {
+
+    // console.log(this.moderationText.value);
+
     //Tolerance
-    const threshold = 0.9;
+    const threshold = 0.01;
 
     //calling a method from our third-party library
     toxicity.load(threshold, []).then(model => {
 
       //now we use our model, parametrized
-      model.classify("You suck").then(predictions => {
-        //Printing out the final result
-        console.log(predictions);
-        // console.log(predictions[0].results[0].match);
+      model.classify(`${this.moderationText.value}`).then(predictions => {
+
+        const aux = predictions.filter((elem: any) => elem.results[0].match);
+
+        // console.log(aux);
+
+        //Gathering the insults
+        let aux_text = "";
+        aux.forEach((elem) => { aux_text = aux_text + ", " + elem.label });
+        // console.log(aux_text);
+        this.moderation = aux_text !== "" ? aux_text : "I was unable to make the judgement, sorry.😅 To me, you are 😇";
+
+
+
+
       })
 
     })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular material
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+//Reactive forms
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //Angular material
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+
+    //Reactive forms
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
